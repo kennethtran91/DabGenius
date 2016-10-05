ecb0a76de4c465ba101e717e141d4358d4abe801
Jeffrey Du <jd479@cornell.edu>
2016-10-05 17:56:28 -0400

    remove session form container from root

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import App from './app';
-import SessionFormContainer from './session/session_form_container';
 
 const _redirectIfLoggedIn = () => {
   if (window.currentUser) {
@@ -14,8 +13,6 @@ const Root = ({ store }) => (
   <Provider store={store}>
     <Router history={hashHistory}>
       <Route path="/" component={App}>
-        <Route path="/login" component={SessionFormContainer} onEnter={_redirectIfLoggedIn}/>
-        <Route path="/signup" component={SessionFormContainer} onEnter={_redirectIfLoggedIn}/>
       </Route>
     </Router>
   </Provider>
