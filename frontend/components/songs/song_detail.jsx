ab2e48f43d43553af0953f6bf2ae41272281ecd7
Jeffrey Du <jd479@cornell.edu>
2016-10-07 14:54:36 -0400

    style for song detail page

--- frontend/components/songs/song_detail.jsx
+++ frontend/components/songs/song_detail.jsx
@@ -9,6 +9,10 @@ class SongDetail extends React.Component {
     this.props.requestOneSong(this.props.params.songId);
   }
 
+  processLyrics() {
+
+  }
+
   render() {
 
     if (this.props.song) {
@@ -16,9 +20,27 @@ class SongDetail extends React.Component {
         <section className="song-detail-container">
           <div className="song-banner">
             <img className="zoomed-banner" src={this.props.song.image_url}></img>
+            <div className="banner-info">
+              <h2 className="song-title">
+                {this.props.song.title}</h2>
+              <br/>
+              <h2 className="song-artist">
+                {this.props.song.artist}</h2>
+            </div>
+            <img className="pro-pic"
+              src={this.props.song.image_url}></img>
           </div>
+
+          <section className="lyrics-container">
+            <h3 className="lyrics-header">
+              {this.props.song.title} lyrics
+            </h3>
+            <p className="lyrics-text">
+              {this.props.song.lyrics}</p>
+          </section>
         </section>
       );
+      
     } else {
       return (
         <div></div>
