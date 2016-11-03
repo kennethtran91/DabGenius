e7f34b3b8659c38c6bdaad7be0d1e3924a98a318
jd479 <jd479@cornell.edu>
2016-11-03 12:02:35 -0400

    add more links to foooter

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -9,12 +9,14 @@ class Footer extends React.Component {
     return (
       <footer className="footer">
         <nav className="footer-nav group">
-          <ul className="footer-links">
-            <li>
-              <a href="https://github.com/jd479/DabGenius">
-                <img src={window.github} className="github-logo"/></a>
-            </li>
-          </ul>
+          <div className="footer-links">
+            <a href="https://github.com/jd479/DabGenius">
+              <img src={window.github} className="github-logo"/></a>
+            <a href="http://jeffdu.com/">
+              <img src={window.about} className="github-logo"/></a>
+            <a href="https://www.linkedin.com/in/jeffreydu">
+              <img src={window.linkedin} className="github-logo"/></a>
+          </div>
         </nav>
       </footer>
     );
